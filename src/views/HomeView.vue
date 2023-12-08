c60e34533ff12b7aae0b137d5289fbd61ced344d
Vikrant Malla <vikrantmalla999@gmail.com>
2023-12-08 21:06:18 +0545

    feat: add a home and about pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Hello this is a home page</h1>
   </main>
 </template>
