bd052f331d9499b7fc413ee2233a1169ee6580e5
Vikrant Malla <vikrantmalla999@gmail.com>
2023-12-14 20:55:26 +0545

    feat: add a dummy api call

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 
 <template>
   <Navbar />
-  <HomeView />
-  <WatchListView />
+  <div>
+    <div v-if="data">
+      <h2>{{ data.title }}</h2>
+      <p>{{ data.body }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 import Navbar from "./components/NavBar.vue"
-import HomeView from "./views/HomeView.vue"
-import WatchListView from "./views/WatchListView.vue"
+
+const data = ref(null);
+
+const fetchData = () => {
+  axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+      data.value = response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+};
+
+onMounted(() => {
+  fetchData();
+});
 </script>
