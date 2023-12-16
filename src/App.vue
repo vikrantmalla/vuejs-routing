6b4b152a28655e3a237a76e588b20e4a87883f8d
Vikrant Malla <vikrantmalla999@gmail.com>
2023-12-16 21:39:04 +0545

    feat: integrate a coincap api and add a table UI

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
 
 <template>
   <Navbar />
-  <div>
-    <div v-if="data">
-      <h2>{{ data.title }}</h2>
-      <p>{{ data.body }}</p>
-    </div>
-  </div>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
 import Navbar from "./components/NavBar.vue"
-
-const data = ref(null);
-
-const fetchData = () => {
-  axios.get('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => {
-      data.value = response.data;
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-};
-
-onMounted(() => {
-  fetchData();
-});
 </script>
